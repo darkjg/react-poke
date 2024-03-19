11bed6bd29d74db5e15007ab0de3c97c7519142b
Jorge <newdarkjg@gmail.com>
2024-03-19 20:45:02 +0100

    Buscardor funcional

--- src/Componentes/Home.jsx
+++ src/Componentes/Home.jsx
@@ -1,8 +1,62 @@
-function Home(){
-    return(
-      <>
-        Hola mundo
-      </>
+import { useState, useEffect } from 'react';
+import styles from "./Home.module.css"
+
+function Busqueda() {
+    const [nombre, setNombre] = useState('');
+    const [mostrarDatos, setMostrarDatos] = useState(null);
+    const [mostrarImg, setMostrarImg] = useState(null);
+
+    useEffect(() => {
+        async function startFetching() {
+            setMostrarDatos(null)
+            try {
+                const respuesta = await fetch(`https://pokeapi.co/api/v2/pokemon/${nombre}`);
+                if (respuesta.ok) {
+                    const datos = await respuesta.json();
+                 
+                    if (datos.name) {
+                        setMostrarDatos(`Nombre ${datos.name}`)
+                    }
+                    if (datos.sprites) {
+                        setMostrarImg("" + datos.sprites.front_default);
+                    }
+
+
+                } else {                 
+                    setMostrarDatos(["Pokemon no encontrado"])
+                    setMostrarImg(null)
+                }
+            } catch (error) {
+                setMostrarDatos(["Error conexion Api" + error])
+                setMostrarImg(null)
+            }
+        }
+        startFetching();
+
+    }, [nombre])
+
+    const handleChange = (e) => {
+        e.preventDefault();
+        setNombre(e.target.value);
+    };
+
+    return (
+        <>
+            <h1>Buscar pokemons</h1>
+            <form>
+                <input type="text" placeholder="Ingresa el nombre del Pokémon" value={nombre} onChange={handleChange} />
+            </form>
+            <h2>{[mostrarDatos] ?? 'Loading...'}</h2>
+            <img src={[mostrarImg] ?? ''}></img>
+        </>
     )
-  }
-  export default Home;
+
+}
+
+
+
+export default Busqueda;
+
+
+
+/**     */
